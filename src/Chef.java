7d826610bd6edfc397536a6db385216c4bd6d180
Snyxxx <samueleulberg@gmail.com>
2023-02-21 18:44:26 +0100

    k

--- src/Chef.java
+++ src/Chef.java
@@ -2,11 +2,17 @@ public class Chef {
     private Personal pers01;
     private Personal pers02;
 
-
-    public Chef(){
+    public Chef(Personal pers01, Personal pers02) {
+        this.pers01 = null;
+        this.pers02 = null;
 
     }
-    public void neuesPersonal(Personal einP){
 
+    public void neuesPersonal(Personal einP) {
+        if (this.pers01 == null) {
+            this.pers01 = einP;
+        } else if (this.pers02 == null) {
+            this.pers02 = einP;
+        }
     }
 }
