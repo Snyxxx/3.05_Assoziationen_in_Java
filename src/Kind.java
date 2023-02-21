1109f075ab7cc1fe5999bc9794f9fb6022797ff9
Snyxxx <samueleulberg@gmail.com>
2023-02-21 19:26:46 +0100

    k

---
+++ src/Kind.java
@@ -0,0 +1,6 @@
+public class Kind {
+
+    private Mutter mama;
+
+    public Kind(Mutter mom) {this.mama = mom; }
+}
---
+++ src/Mensch.java
@@ -0,0 +1,12 @@
+public class Mensch {
+
+    private Ehepartner partner;
+
+    public Mensch(Ehepartner partner) {
+        this.partner = null;
+    }
+
+    public void heirat(Ehepartner partner) {
+        this.partner = partner;
+    }
+}
